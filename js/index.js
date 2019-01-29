e11e93a32525c25be843e6aae7f3332695e8b7ae
olympiawojcik <owojcik7@gmail.com>
2019-01-29 17:58:06 -0500

    add event stop propogation in index.js

--- js/index.js
+++ js/index.js
@@ -9,15 +9,24 @@ logoHeading.addEventListener("mouseover", event => {
 //2) Click on Sign-up buttons (Challenge: How to do this in a forEach?)
 const buttonEl = document.querySelectorAll(".btn")
 buttonEl[0].addEventListener("click", e => {
+    e.stopPropagation();
     console.log(`this is my event: ${event}`)
 })
 buttonEl[1].addEventListener("click", e => {
+    e.stopPropagation();
     console.log(`this is my event: ${event}`)
 })
 buttonEl[2].addEventListener("click", e => {
+    e.stopPropagation();
     console.log(`this is my event: ${event}`)
 })
 
+
+//2) Event Prop -- Click anywhere in body
+const bodyTest = document.querySelector("body")
+bodyTest.addEventListener("click", event => {
+    console.log("Body Test container event fired")
+})
 
 //3) Keydown on entire doc
 document.addEventListener("keydown", event => {
@@ -27,6 +36,32 @@ document.addEventListener("keydown", event => {
 //4 Load on entire window
 window.addEventListener("load", function(event) {
     console.log("All resources finished loading!");
+})
+
+//5 Mousedown on img
+const funBusImg = document.querySelector("header img")
+
+funBusImg.addEventListener("mousedown", function(event) {
+    funBusImg.remove();
+    console.log('The fun bus image was removed')
+})
+
+//6 Resize 
+window.addEventListener("resize", event => {
+    console.log("The window has been resized")
+    
+}, {once: true})
+
+//7 Dblclick on copyright
+const copyright = document.querySelector(".footer p")
+window.addEventListener('dblclick', event => {
+    copyright.style.color = 'red';
+})
+
+//8 Select on any text in P
+const h2 = document.querySelector("h2") 
+h2.addEventListener("onselect", event => {
+    alert('Youve selected some text')
 })
 
 /*	* [ ] `mouseover`
